b4c6a7907e47dec76026fb17b3495b36fa3d60d4
Ilkka Kettunen <ilkkakettunen@Ilkkas-Mac-mini.local>
2024-11-19 17:09:28 +0200

    Updates

---
+++ precache-manifest.1192c128fde2a6646d4e4d2042e6d02b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b977ec401bbb9e18ac71b14bafc5315a",
+    "url": "/cv/index.html"
+  },
+  {
+    "revision": "2d524a62142b3810f11c",
+    "url": "/cv/static/css/main.435d5fe5.chunk.css"
+  },
+  {
+    "revision": "1582e325d1bcf091e744",
+    "url": "/cv/static/js/2.ca150098.chunk.js"
+  },
+  {
+    "revision": "2e7b5277f70c872aecfe",
+    "url": "/cv/static/js/3.940f5f6f.chunk.js"
+  },
+  {
+    "revision": "2d524a62142b3810f11c",
+    "url": "/cv/static/js/main.6a80a607.chunk.js"
+  },
+  {
+    "revision": "07912d8b2c214d6bfa55",
+    "url": "/cv/static/js/runtime~main.1aadae83.js"
+  }
+]);
